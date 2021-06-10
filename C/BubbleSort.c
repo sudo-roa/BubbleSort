3f87850a17d31face0dfe6ec8bc8b01c9a22804d
suzu-akkikki <henattyo5.9@gmail.com>
2021-06-10 13:26:58 +0900

    fix BubbleSort.c

--- C/BubbleSort.c
+++ C/BubbleSort.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-    int tmp;
-    int i,j;    
+    int tmp=0;
+    int i,j=0;    
     int data[] = {54,78,12,2,89,25,78,29,69,35};
     int dataNumber = sizeof data / sizeof data[0];
     for(i=0;i<10;i++){
-        printf("%d ", data[i]);
+	printf("%d ", data[i]);
     }
     printf("\n");
 
-    for(i=0;i<dataNumber;i++){
-        for(j=0;j<dataNumber-i;j++){
-            if(data[j]>data[j+1]){
+    for(i=dataNumber-1;i>0;i--){
+        for(j=0;j<i;j++){
+	    if(data[j]>data[j+1]){
                 tmp = data[j];
                 data[j] = data[j+1];
                 data[j+1] = tmp;
